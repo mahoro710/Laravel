7df0b04141da6194c2463d138e5dd2887111c721
mahoro710 <mahoro710@gmail.com>
2022-04-10 02:43:25 +0900

    適切なコミットメント

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','PostController@index');
+Route::get('/posts/{post}','PostController@show');
